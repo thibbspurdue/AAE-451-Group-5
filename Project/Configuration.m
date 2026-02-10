ad7760d965fd39933ee84fb6b8b1d80d8ac29524
cubicalCroissant <chinyelhuang@gmail.com>
2026-02-10 15:10:27 -0500

    assignment 4 test

---
+++ Project/Configuration.m
@@ -0,0 +1,37 @@
+classdef Configuration
+    % CONFIGURATION Defines a Configuration object containing information
+    % to calculate physical properties such as lift and drag.
+    %   Detailed explanation goes here
+    
+    properties
+        aspect_ratio
+        fuselage_diameter
+        wingspan
+        
+    end
+    
+    methods
+        function obj = Configuration(inputArg1,inputArg2)
+            %DRAG Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function C_L = calculateLift(obj, AoA)
+            
+            C_L = 
+        end
+
+        function C_D0 = parasiticDrag(obj)
+            C_D0 = 
+        end
+
+        function totalDrag = method1(obj, inputArg)
+            % TOTALDRAG Calculates the total drag coefficient of an
+            % aircraft at a specific angle of attack
+            %   C_D = C_D0 + (C_L^2 / (pi * AR * e)) + C_DW
+            totalDrag = parasitic_drag + ();
+        end
+    end
+end
+
