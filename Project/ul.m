223c9ab0ef23153341d51e3f0ec2af3379352d54
U511 <U511@mikan>
2026-02-19 08:37:06 +0900

    Add native conversion to SI units and allow passthrough of unitless variables

--- Project/ul.m
+++ Project/ul.m
@@ -1,5 +1,13 @@
 function output = ul(input)
 % UL Removes units from a symbolic variable and returns the unitless
-% expression as a double.
-    output = double(simplify(separateUnits(input)));
+% expression as a double. Returns the unmodified argument if no units are
+% attached.
+% 
+% https://www.mathworks.com/help/symbolic/units-list.html#mw_6567b974-b766-4942-bf6d-d787c12778e3
+    [~, unitList] = separateUnits(input);
+    if unitList.size > 0
+        output = input;
+    else
+        output = double(separateUnits(simplify(unitConvert(input, 'SI', 'Derived'))));
+    end
 end
