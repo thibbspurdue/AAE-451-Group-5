eb6bd08eaf6ecd14ec2d8b6e1ca91cde9d785a2b
thibbspurdue <thibbs@purdue.edu>
2026-02-18 10:06:48 -0500

    Reworked the input system and cleaned up some variables. STILL A DRAFT

--- Assignment 8/AAE451_Assignment_8.m
+++ Assignment 8/AAE451_Assignment_8.m
@@ -13,36 +13,63 @@ end
 aircraft = 'Sunfish';
 
 %Aircraft
-fields = {'W','AR', 'Swp', 'c_bar', 'b_w', 'S_w', 'l_t', 'S_t', 'AR_t', 'e_t', 'l_v', 'S_v', 'C_L_0', 'C_L_a', 'C_L_d', 'C_M_0', 'C_M_a', 'C_M_d', 'a_s', 'C_L_a_t', 'C_M_a_t', 'C_D_0', 'x_ac', 'n_h', 'de_da', 'C_L_max', 'C_L_rot', 'C_L_t_NU', 'C_M_RRo','SM','S'};
-% THREE MORE VARIABLES, ENSURE IT REFLECTS ON THE SPREADSHEET
-units = [u.kg, 1, u.deg, u.m, u.m, u.m^2, u.m, u.m^2, 1,1,u.m,u.m^2, 1, 1/u.rad, 1/u.rad, 1, 1/u.rad, 1/u.rad, u.rad, 1/u.rad, 1/u.rad, 1, u.m,1,1,1,1,1,1,1,u.m^2];
+fields_lengths = {'c_bar', 'c_r','c_ht', 'c_vt', 'b_w', 'l_t','l_v', 'l_f', 'd_f', 'l_N', 'd_N'};
+u_lengths = u.m * ones(length(fields_lengths),1);
+
+fields_areas = {'S_w', 'S_t','S_v', 'S'};
+u_areas = u.m^2 * ones(length(fields_areas),1);
+
+fields_angles = {'Swp_w', 'Swp_ht', 'Swp_vt', 'a_s'};
+u_rad = u.rad * ones(length(fields_angles),1);
+
+    %nondimensional
+fields_nondim = {'AR','AR_ht', 'AR_vt','e_t','C_L_0','C_L_0_t', 'C_M_0','x_ac', 'n_h','C_L_max', 'C_L_rot', 'C_L_t_NU', 'C_M_RRo','SM', 'Qf', 'Qw', 'Qht', 'Qvt', 'Qn', 'E'};
+u_nd = ones(length(fields_nondim),1);
+
+    %1/rad
+fields_airfoil_inv_rads = {'C_L_a', 'C_L_d', 'C_M_a', 'C_M_d','C_L_a_t', 'C_M_a_t'};
+u_inv_rad = (1/u.rad) * ones(length(fields_airfoil_inv_rads),1);
+
+fields_weights = {'W'};
+u_w = u.kg * ones(length(fields_weights),1);
+%append all into one
+fields = [fields_lengths, fields_areas, fields_angles, fields_nondim, fields_airfoil_inv_rads, ...
+          fields_weights];
+
+units = [u_lengths; u_areas; u_rad; u_nd; u_inv_rad; u_w];
+
+%deal with reading in the multiple sheets later
 T = readtable('Parameters_copy.xlsx','Sheet',aircraft);
 data = vpa(table2array(T)) .* units;
 x = cell2struct(num2cell(data), fields, 2);
+
+
 % % Now you access them via x.AR, x.c_bar, etc.
-c_e = 0; %PLACEHOLDER
-c_t = 0;
-C_L_de_t = 0;
-C_L_t_De = 0;
-C_L_0_t = 0;
-K = 0;
 fieldNames = fieldnames(x);
 
 for i = 1:numel(fieldNames)
     assignin('base', fieldNames{i}, x.(fieldNames{i}));
 end
 
+%% Currently Overriding the Static Margin given in the sheet
+SM = 0;
+
+n_h = 0.9; %overriding the n value cause idk
+
+%% Step 0.5, call the drag complex function
+%assumptions made:
+%1. There is no variation for QCS, QCS=LES=TES
+Q = [Qf Qw Qht Qvt Qn];
+QCS_w = Swp_w;
+QCS_ht = Swp_ht;
+QCS_vt = Swp_vt;
+S_ref = S;
+C_D0 = Drag_Complex(Q, l_f, d_f, l_N, d_N, QCS_w, QCS_ht, QCS_vt, t_c_w, t_c_ht, t_c_vt, c_bar, c_r, c_ht, c_vt, S_ref, S_w, S_ht, S_vt);
 %% 2.a  geometry, horizontal tail
 V_h = S_t * l_t / S_w / c_bar;
 
 %% 2.b  Scissor Plot
 n = 100; %number of points
-
-%needed variables
-% n_h = ; %n_horiontal tail
-% de_da = ;
-% C_L_max = ;
-% C_L_rot = ;
 
 
     %x var is x_cg/c_bar
@@ -54,6 +81,7 @@ x_cg = x_cg_c_bar * c_bar;
 C_L_t_ND = C_L_a_t * a_s; %Coefficent of lift for the tail if nose down
 C_M_RR = -C_L_max * SM + C_M_0; %Coefficent of moment for required recovery
 
+de_da = 2 * C_L_a / pi / AR;
 
     %Parameters for Takeoff Rotation
 % C_L_t_NU = ; %Maximum negative tail lift for nose-up control (-0.5 ~ -0.8)
@@ -89,12 +117,12 @@ C_L_trim = linspace(-1, 2, n);
 
 %Slide 23 semi-nonsense
 %% I dont know the difference between C_L_de_t and C_L_t_de
-E = c_e / c_t; %This is an airfoil thing
+%E = c_e / c_t; %This is an airfoil thing
 C_L_t_de = (C_L_a_t/pi) * (acos(1 - 2*E) + 2 * sqrt(E * (1 - E)));
-C_M_t_de = (C_M_a_t / pi) * (1 - E) * sqrt(2*E - 1);
+C_L_de_t = (C_M_a_t / pi) * (1 - E) * sqrt(2*E - 1);
 
 C_L_de = (S_t / S_w) * C_L_de_t;
-C_M_de = C_L_t_De * (S_t / S_w) * (x_cg - x_ac) - C_L_de_t * V_h;
+C_M_de = C_L_t_de * (S_t / S_w) * (x_cg - x_ac) - C_L_de_t * V_h;
 
 %Slide 22, less semi-nonsense
 a_trim = (C_M_0 * C_L_de + C_M_de .* (C_L_trim - C_L_0)) ./ (C_L_a * C_M_de - C_L_de * C_M_a);
@@ -118,8 +146,17 @@ C_L_wing = C_L_0 + C_L_a * a_trim;
 C_L_tail_clean = C_L_0_t + C_L_a_t * a_trim;
 C_L_tail = C_L_tail_clean + C_L_de * de_trim;
 
-C_D_clean = C_D_0 + K * C_L_wing.^2+ (S_t / S) * C_L_tail_clean.^2 / (pi * e_t * AR_t);
-C_D_trimmed = C_D_0 + K * C_L_wing.^2+ (S_t / S) * C_L_tail.^2 / (pi * e_t * AR_t);
+%define K in the same way that the K is defined for the tail
+e = 4.61 * (1 - 0.045*AR^0.68) * (cos(Swp_w)^0.15) - 3.1; %taken from a4
+K = 1 / (pi * AR * e);
+
+if(e_t == 0) %using the horizontal tail only, someone else can change from here if they wnt
+    e_t = 4.61 * (1 - 0.045*AR_ht^0.68) * (cos(Swp_ht)^0.15) - 3.1; %taken from a4
+end
+K_t = 1 / (pi * e_t * AR_ht);
+
+C_D_clean = C_D_0 + K * C_L_wing.^2 + K_t * (S_t / S) * C_L_tail_clean.^2;
+C_D_trimmed = C_D_0 + K * C_L_wing.^2 + K_t * (S_t / S) * C_L_tail_clean.^2;
 
 figure;
 plot(C_L_trim, C_D_clean);
