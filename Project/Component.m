a67cd9de2759d1a664af8b442a3c633c6a8bdb0a
U511 <U511@mikan>
2026-02-19 17:04:17 +0900

    Separate out static method and refactor templates for inheriting component classes

--- Project/Component.m
+++ Project/Component.m
@@ -1,50 +1,54 @@
 classdef Component
-    % COMPONENT Generic component used for drag analysis.
-    % Common components such as wings, fuselages, nacelles, etc. should use
-    % their respective subclasses.
-    
-    properties (Constant)
-        tempunits = symunit;
-        g (1,1) double {mustBePositive} = 9.81 % gravitational acceleration
-    end
+    % COMPONENT Generic component.
+    % Common components such as wings, fuselages, nacelles, etc. should be
+    % instantiated using respective subfunctions to ensure correct
+    % attributes are defined.
+
     properties
         fineness_ratio = 0
         form_factor = 0
         interference_factor = 0
         wetted_area = 0
+        mass = 0
     end
-    
+    % properties (Dependent)
+    %     wetted_area
+    % end
+
     methods
-        function obj = Component(form_factor, interference_factor, wetted_area)
+        function obj = Component(interference_factor, form_factor, wetted_area)
             % COMPONENT Construct a generic Component object.
-            % Used in specific component subclasses.
-            % arguments
-            %     form_factor
-            %     interference_factor
-            %     % skin_friction_factor % not included since this is
-            %     % altitude-dependent as opposed to an intrinsic property
-            %     wetted_area
-            % end
             if nargin == 0
                 return
             end
-
-            if has_units(wetted_area)
-                wetted_area = ul(unitConvert(wetted_area, obj.tempunits.m^2));
-            end
-
-            obj.form_factor = form_factor;
             obj.interference_factor = interference_factor;
-            obj.wetted_area = wetted_area;
+            obj.form_factor = form_factor;
+            obj.wetted_area = ul(wetted_area);
         end
 
-        function re = calc_reynolds_no(length, altitude)
+        function re = reynolds_number(length, altitude, velocity)
+            [length, altitude, velocity] = ul([length, altitude, velocity]);
             re = length * velocity * Atm.density(altitude) / Atm.viscosity_dyn(altitude);
         end
-        
-        function output = calc_CD0(obj, skin_friction_factor, ref_wing_area)
+
+        function output = CD0(obj, skin_friction_coeff, ref_wing_area)
             % CALC_CD0 Calculates and returns parasitic drag of component
-            output = obj.form_factor * obj.interference_factor * skin_friction_factor * obj.wetted_area / ref_wing_area;
+            arguments
+                obj
+                skin_friction_coeff {mustBePositive}
+                ref_wing_area {mustBePositive}
+            end
+            ref_wing_area = ul(ref_wing_area);
+            output = obj.form_factor * obj.interference_factor * skin_friction_coeff * obj.wetted_area / ref_wing_area;
+        end
+    end
+
+    methods (Static)
+        function output = skin_friction_coeff(reynolds_number)
+            % CALC_SKIN_FRICTION_COEFF Returns skin friction coefficient as
+            % determined using simplified 'Schlichting Formula' from Week 3
+            % Slide 11. Requires Reynolds number.
+            output = 0.455 / (log10(reynolds_number)^2.58);
         end
     end
 end
